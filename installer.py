9bfbb59fd55ffbaf7b7d3e21f08b5a73f196ea7f
ResearchForumOnline <researchforumonline@users.noreply.github.com>
2026-09-21 21:58:11 +0100

    Release 0.1.3 native model and review improvements

--- installer.py
+++ installer.py
@@ -8,8 +8,8 @@ from pathlib import Path
 import tkinter as tk
 from tkinter import messagebox, ttk
 
-VERSION = '0.1.2'
-ASSET = 'TalkToAi-Code-0.1.2-Windows-Portable.zip'
+VERSION = '0.1.3'
+ASSET = 'TalkToAi-Code-0.1.3-Windows-Portable.zip'
 URL = f'https://github.com/ResearchForumOnline/TalkToAi-Code/releases/download/v{VERSION}-preview/{ASSET}'
 
 UNINSTALL = r'''param([switch]$RemoveUserData)
